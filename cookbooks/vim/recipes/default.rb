6e06c8b77339df7b3df55c4da10a64191a0fcd48
skim <skim.la@gmail.com>
2012-11-15 17:23:06 -0600

    updates vim recipe to point sl4m/vim_dotfiles

--- cookbooks/vim/recipes/default.rb
+++ cookbooks/vim/recipes/default.rb
@@ -1,61 +1,33 @@
+vim_dir = Helper.home('.vim')
+vim_dotfiles_dir = Helper.home('.vim_dotfiles')
+
 package 'vim-nox'
 
-user_cookbook_file '.vimrc'
-
-vim_dir = Helper.home('.vim')
-
-user_remote_directory vim_dir do
-  overwrite true
-  purge true
-  recursive true
+git vim_dotfiles_dir do
+  repository 'https://github.com/sl4m/vim_dotfiles.git'
+  reference 'master'
+  action :checkout
+  enable_submodules true
 end
 
-bundle_dir = "#{vim_dir}/bundle"
+link "#{vim_dir}/.vimrc" do
+  Helper.user
+  to "#{vim_dotfiles_dir}/vimrc"
+  not_if "test -e #{vim_dir}/.vimrc"
+end
 
-user_directory bundle_dir
-
-git "#{bundle_dir}/command-t" do
-  repository 'https://github.com/wincent/Command-T.git'
-  reference 'master'
-  action :sync
+link "#{vim_dir}/.vim" do
+  Helper.user
+  to "#{vim_dotfiles_dir}/vim"
+  not_if "test -e #{vim_dir}/.vim"
 end
 
 bash 'build command-t' do
   code <<-EOH
-  cd #{bundle_dir}/command-t/ruby/command-t
+  cd #{vim_dotfiles_dir}/vim/bundle/command-t/ruby/command-t
   rvm use system
   ruby extconf.rb
   make clean
   make
   EOH
-end
-
-git "#{bundle_dir}/vim-fugitive" do
-  repository 'https://github.com/tpope/vim-fugitive.git'
-  reference 'master'
-  action :sync
-end
-
-git "#{bundle_dir}/vim-ruby" do
-  repository 'https://github.com/vim-ruby/vim-ruby.git'
-  reference 'master'
-  action :sync
-end
-
-git "#{bundle_dir}/nerdtree" do
-  repository 'https://github.com/scrooloose/nerdtree.git'
-  reference 'master'
-  action :sync
-end
-
-git "#{bundle_dir}/vim-clojure" do
-  repository 'https://github.com/vim-scripts/VimClojure.git'
-  reference 'master'
-  action :sync
-end
-
-git "#{bundle_dir}/vim-coffee-script" do
-  repository 'https://github.com/kchmck/vim-coffee-script.git'
-  reference 'master'
-  action :sync
 end
