1e4c1b5ddf4596bd2f0c9549ea4dd270b0f866f0
skim <skim.la@gmail.com>
2016-04-13 11:22:34 +0100

    updates tmux recipe to build 2.2

--- cookbooks/default/tmux/recipes/default.rb
+++ cookbooks/default/tmux/recipes/default.rb
@@ -1,4 +1,32 @@
-apt_package 'tmux'
+tmux_version       = '2.2'
+tmux_url           = "https://github.com/tmux/tmux/releases/download/#{tmux_version}/tmux-#{tmux_version}.tar.gz"
+tmux_tar           = Helper.home("tmux-#{tmux_version}.tar.gz")
+tmux_unpacked_path = Helper.home("tmux-#{tmux_version}")
+
+remote_file tmux_tar do
+  owner Helper.user
+  source tmux_url
+  mode '0644'
+end
+
+bash 'unpack tmux' do
+  user Helper.user
+  group Helper.group
+  code "tar -xzvf #{tmux_tar} -C #{Helper.home}"
+end
+
+file tmux_tar do
+  owner Helper.user
+  action :delete
+end
+
+bash 'install tmux' do
+  code <<-EOH
+  cd #{tmux_unpacked_path}/
+  ./configure && make
+  sudo make install
+  EOH
+end
 
 cookbook_file Helper.home('.tmux.conf') do
   owner Helper.user
