256c448132fc0c925531b68ffdddce2280c0368e
skim <skim.la@gmail.com>
2016-09-14 12:10:24 +0100

    updates elasticsearch recipe

--- cookbooks/default/elasticsearch/files/default/elasticsearch.zsh
+++
@@ -1,1 +0,0 @@
-export PATH=$ES_HOME/bin:$PATH
--- cookbooks/default/elasticsearch/recipes/default.rb
+++ cookbooks/default/elasticsearch/recipes/default.rb
@@ -1,56 +1,7 @@
-elasticsearch_package       = 'elasticsearch-1.1.0'
-elasticsearch_url           = "https://download.elasticsearch.org/elasticsearch/elasticsearch/#{elasticsearch_package}.tar.gz"
-elasticsearch_tar           = Helper.home('elasticsearch.tar.gz')
-elasticsearch_home          = Helper.home('.elasticsearch')
-elasticsearch_unpacked_path = Helper.home(elasticsearch_package)
+cookbook_file '/etc/apt/sources.list.d/elasticsearch.list'
 
-remote_file elasticsearch_tar do
-  owner Helper.user
-  source elasticsearch_url
-  mode '0644'
+bash 'apt key/apt-get update' do
+  code Helper.apt_key('https://packages.elastic.co/GPG-KEY-elasticsearch')
 end
 
-directory elasticsearch_home do
-  owner Helper.user
-  action :delete
-  recursive true
-end
-
-bash 'unpack elasticsearch' do
-  user Helper.user
-  group Helper.group
-  code "tar -xzvf #{elasticsearch_tar} -C #{Helper.home}"
-end
-
-bash 'move elasticsearch' do
-  user Helper.user
-  group Helper.group
-  code "mv #{elasticsearch_unpacked_path} #{elasticsearch_home}"
-end
-
-file elasticsearch_tar do
-  owner Helper.user
-  action :delete
-end
-
-cookbook_file Helper.home('.zsh/elasticsearch.zsh') do
-  owner Helper.user
-  group Helper.group
-end
-
-cookbook_file File.join(elasticsearch_home, 'elasticsearch.yml') do
-  owner Helper.user
-  group Helper.group
-end
-
-cookbook_file '/etc/init.d/elasticsearch' do
-  mode 0755
-end
-
-bash 'up the amount of open files for elasticsearch' do
-  code "sudo sed -i '$ a\* soft nofile 32000' /etc/security/limits.conf"
-end
-
-bash 'initialize init daemon script' do
-  code 'sudo update-rc.d elasticsearch defaults'
-end
+apt_package 'elasticsearch'
